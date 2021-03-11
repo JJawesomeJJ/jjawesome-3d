701eed3b5cce87830d1794e4cae95b4c8ff5a4da
zhaolijie <xuzhenzhe@xbcx.com>
2021-03-11 21:02:37 +0800

    -water----normal life -- --single life how long will continue --No mater how sadness of the life remember you are petty unique boy --fight young man the futher may be darkness but don't be fear,remember the beauty will always wait for you in the some which is not far away --2020-12-15

--- src/components/pages/lib/road.js
+++ src/components/pages/lib/road.js
@@ -5,12 +5,16 @@ import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
 import Base3d from "./Base3d";
 import baseURL from "../../../config/config";
 import Timer from "./until/Timer";
+import roadImg from "../../../assets/images/road.png"
 export default class road extends Base3d{
   init() {
+    let roadTexture = new THREE.TextureLoader().load(roadImg);
+    roadTexture.wrapS = roadTexture.wrapT = THREE.RepeatWrapping;
     this.timer=Timer.singleTon();
     this.roadMaterial=new THREE.ShaderMaterial({
       // depthWrite: true,
       depthTest: true,
+      opacity:0.8,
       polygonOffset: true,
       polygonOffsetFactor: -5.0,
       polygonOffsetUnits: 2.0,
@@ -59,34 +63,62 @@ export default class road extends Base3d{
           float animate(){
               return step(abs(0.5-uVu.x),uStartx);
           }
-          float getLightLine(float height){
-                getEdgeLightColor(0.1)*step(0.5-height,uStartX-abs(uVu.x-0.5));
+          float getLightLine(float height,vec4 edgeColor){
+               return hasColor(edgeColor.xyz)*animate();
+          }
+          vec4 getPlusLight(){
+               return step(1.0,uStartx)*step(abs(0.5-uVu.y),(uStartx-1.0)*3.0)*vec4(1.0,1.0,1.0,1.0)*0.2;
+          }
+          vec4 getFirstLightColor(float height,float hasColorEdgeColor){
+               float currentX=abs(0.5-uVu.x);
+               return step(uStartx-currentX,height)*vec4(1.0,0.0,0.0,1.0)*hasColorEdgeColor;
           }
           void main(){
               vec4 edgeColor=getEdgeLightColor(0.1);
+              vec4 firstLightColor=getFirstLightColor(0.05,hasColor(edgeColor.xyz));
               gl_FragColor=edgeColor+getBaseColor(edgeColor);
+              gl_FragColor+=getPlusLight();
+              gl_FragColor=gl_FragColor*(1.0-hasColor(firstLightColor.xyz))+firstLightColor;
               gl_FragColor=gl_FragColor*animate();
-              gl_FragColor+=hasColor(edgeColor.xyz)*step(abs(0.5-uVu.x),uLightX-0.1)*vec4(1.0,1.0,1.0,1.0)*0.2+getLightLine(0.08)*vec4(1.0,0.0,0.0,1.0);
+               gl_FragColor.w*= 0.7;
           }
         `,
     })
     super.init();
-    let path=baseURL+"/static/models/shixinlu.fbx";
-    //var loader = new FBXLoader();
-    // loader.load(path,(obj)=>{
-    //   obj.traverse((ele)=>{
-    //     ele.scale.setY(0.01)
-    //     ele.material=
-    //   })
-    //   this.scene.add(obj)
-    // })
+    let path=baseURL+"/static/models/HX1(1).FBX";
+    var loader = new FBXLoader();
+    loader.load(path,(obj)=>{
+      let objClone=obj.clone();
+      obj.rotateX(Math.PI/2)
+      obj.scale.set(0.5,0.5,0.5)
+      obj.traverse((ele)=>{
+        //ele.scale.setY(0.01)
+        ele.material=this.roadMaterial;
+      })
+      obj.traverse((ele)=>{
+        //ele.scale.setY(0.01)
+        ele.material=this.roadMaterial;
+      })
+
+      this.scene.add(obj,obj.clone())
+    })
     let mesh=new THREE.Mesh(new THREE.PlaneGeometry(1000,20,30),this.roadMaterial)
-    this.scene.add(mesh)
+    let mesh2=mesh.clone();
+    mesh2.material=new THREE.MeshBasicMaterial({map:roadTexture});
+    mesh2.position.setZ(-5.0);
+    this.scene.add(mesh,mesh2)
   }
   render() {
-    this.roadMaterial.uniforms.uStartx.value=this.timer.getOrSetRunTime("roadName",0.001,this.timer.getAspeendFun(0.01));
-    this.roadMaterial.uniforms.uLightX.value=this.timer.getOrSetRunTime("roadNameLight",0.001,this.timer.getAspeendFun(0.007));
+    if(this.roadMaterial.uniforms.uStartx.value<=2.0){
 
+    }
+    this.roadMaterial.uniforms.uStartx.value=Math.abs(this.timer.getOrSetRunTime("roadName",0.001,this.timer.getSin(0.02)));
+    // if(this.roadMaterial.uniforms.uStartx.value>=2.0){
+    //   this.timer.stop("roadName")
+    // }
+    // if (this.roadMaterial.uniforms.uStartx.value<=0.0){
+    //   this.timer.restart("roadName")
+    // }
     super.render();
   }
 }
--- src/components/pages/lib/until/Timer.js
+++ src/components/pages/lib/until/Timer.js
@@ -6,6 +6,19 @@ export default class Timer{
     }
     return obj;
   }
+
+  /**
+   * 检查某个键是否在正常持续的运行
+   * @param key
+   * @returns {boolean|boolean|number|jest.Status|string|"rejected"|number|"fulfilled"|*}
+   */
+  getKeyIsRunning(key){
+    if(!this.runTime.hasOwnProperty(key)){
+      return false;
+    }else {
+      return this.runTime[key].status;
+    }
+  }
   getOrSetRunTime(key,speed=0.01,fun=null,isReset=true){
     this.runningTime();
     if(!this.runTime){
@@ -18,7 +31,8 @@ export default class Timer{
         startTime:(new Date()).valueOf(),
         currentTime:(new Date()).valueOf(),
         speed:speed,
-        fun:fun
+        fun:fun,
+        status:true
       };
     }
     if(!isReset){
@@ -28,7 +42,8 @@ export default class Timer{
         startTime:(new Date()).valueOf(),
         currentTime:(new Date()).valueOf(),
         speed:speed,
-        fun:fun
+        fun:fun,
+        status:true
       };
     }
     return this.runTime[key].value;
@@ -39,13 +54,23 @@ export default class Timer{
     }
     Object.keys(this.runTime).forEach((key)=>{
       let ele=this.runTime[key];
+      if(!ele.status){
+        return;
+      }
       ele.currentTime=(new Date()).valueOf();
       ele.time = (ele.currentTime-ele.startTime)/100.0;
       if(!ele.fun) {
         ele.value = ele.speed*ele.time;
         this.runTime[key] = ele;
       }else {
-        ele.value=ele.fun({value:ele.value,speed:ele.speed,time:ele.time})
+        let value=ele.fun({value:ele.value,speed:ele.speed,time:ele.time});
+        if(!(value instanceof Object)){
+          ele.value=value;
+        }else {
+          Object.keys(value).forEach((key1)=>{
+            ele[key1]=value[key1]
+          });
+        }
       }
     })
   }
@@ -57,11 +82,42 @@ export default class Timer{
    */
   getAspeendFun(a){
     return function (params){
-      let value=params.value;
       let speed=params.speed;
       let time=params.time;
-      value=speed*time+(1/2)*a*Math.pow(time,2);
+      let value= speed*time+(1/2)*a*Math.pow(time,2);
       return value;
     }
+  }
+
+  /**
+   *
+   * @param a
+   * @param middlevalue 临界值
+   * @returns {function(*)}
+   */
+  getSinValue(a,middlevalue){
+    return function (params){
+
+    }
+  }
+  stop(key){
+    this.getOrSetRunTime(key);
+    this.runTime[key]["status"]=false;
+  }
+  getSin(radio=1.0){
+    return function (params){
+      return Math.sin(params.time*radio);
+    }
+  }
+
+  /**
+   *
+   * @param key
+   */
+  restart(key){
+    this.getOrSetRunTime(key);
+    this.runTime[key]["status"]=true;
+    this.runTime[key]["startTime"]=(new Date()).valueOf();
+    this.runTime[key]["currentTime"]=(new Date()).valueOf();
   }
 }
